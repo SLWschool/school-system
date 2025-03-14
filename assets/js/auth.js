6007a5e1fcf5b060eaaa635d06be661969aeda0b
SLWschool <samosonluangwittayalai@gmail.com>
2025-03-14 16:23:22 +0700

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -7,17 +7,22 @@ function login(event) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // ดึงข้อมูลจาก login.json
+    // ดึงข้อมูลจาก Google Sheets ผ่าน Google Apps Script
     fetch('https://script.google.com/macros/s/AKfycbwuqsiQsOEqEcrRU37qs1WMOwF5f-1yZj3OthEKLYE2rCikvr4-05AsbRniyL7KXDvQpw/exec')
         .then(response => response.json())
         .then(data => {
-            const user = data.find(user => user.username === username); // ใช้ find แทนการใช้ filter
+            const user = data.find(user => user.username === username); // ค้นหาผู้ใช้จาก username
 
             // ตรวจสอบรหัสผู้ใช้
             if (user && user.password === password) {
                 alert('เข้าสู่ระบบสำเร็จ');
-                localStorage.setItem('userRole', user.role); // เก็บบทบาทผู้ใช้ใน localStorage
-                window.location.href = 'dashboard.html';  // ไปที่หน้า dashboard เมื่อเข้าสู่ระบบสำเร็จ
+
+                // เก็บข้อมูลใน localStorage
+                localStorage.setItem('username', user.username);
+                localStorage.setItem('name', user.name);  // เก็บชื่อครู
+                localStorage.setItem('userRole', user.role);
+
+                window.location.href = 'dashboard.html';  // ไปที่หน้า dashboard
             } else {
                 alert('ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง');
             }
