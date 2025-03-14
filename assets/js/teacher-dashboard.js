d989259db0072f2ae1ea4e851090b23753c816ad
SLWschool <samosonluangwittayalai@gmail.com>
2025-03-14 16:46:29 +0700

    Update teacher-dashboard.js

--- assets/js/teacher-dashboard.js
+++ assets/js/teacher-dashboard.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => { 
     const teacherName = localStorage.getItem('name') || localStorage.getItem('username'); // ดึงชื่อเต็มหรือ username
-    document.getElementById('teacher-name').textContent = teacherName || 'ครู';
+    const teacherEmail = localStorage.getItem('username');
+    const teacherRole = localStorage.getItem('userRole');
+    document.getElementById('teacher-name').textContent = teacherName || 'อาจารย์ทุกท่าน';
+    document.getElementById('teacher-email').textContent = teacherEmail || 'contact@slw.ac.th';
+    document.getElementById('teacher-role').textContent = teacherRole || 'ครู';
 
     // ตรวจสอบสิทธิ์การเข้าใช้งาน
     const userRole = localStorage.getItem('userRole');
